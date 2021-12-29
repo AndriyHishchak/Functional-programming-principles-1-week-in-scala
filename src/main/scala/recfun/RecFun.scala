fc56d1668c9de8fd24a51d33721b8c0f2190bb09
andriy hishchak <64685716+AndriyHishchak@users.noreply.github.com>
2021-12-29 15:54:56 +0200

    implemented the pascal method

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -10,9 +10,12 @@ object RecFun extends RecFunInterface:
       println()
 
   /**
-   * Exercise 1
+   * This method keep an index of rows and columns and check if a point is the first or last value in the row
+   * if first or last, point = 1. Else else take position and sum the previous row n & n-1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if(c == 0 || c == r) 1 else pascal(c, r-1) + pascal(c-1, r-1)
+  }
 
   /**
    * Exercise 2
