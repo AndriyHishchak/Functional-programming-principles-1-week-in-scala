5a3f61f2f9f9c1b09d879377b0700a3ccfba5a7c
andriy hishchak <64685716+AndriyHishchak@users.noreply.github.com>
2021-12-29 16:09:58 +0200

    implemented the balance method

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -7,7 +7,15 @@ object RecFun extends RecFunInterface:
     for row <- 0 to 10 do
       for col <- 0 to row do
         print(s"${pascal(col, row)} ")
-      println()
+
+      println("Parentheses Balancing")
+      println(" - " + balance("".toList))
+      println("test - " + balance("test".toList))
+      println("test( - " + balance("test(".toList))
+      println("test() - " + balance("test()".toList))
+      println("test()) - " + balance("test())".toList))
+      println("test())( - " + balance("test())(".toList))
+
 
   /**
    * This method keep an index of rows and columns and check if a point is the first or last value in the row
@@ -18,9 +26,22 @@ object RecFun extends RecFunInterface:
   }
 
   /**
-   * Exercise 2
+   * This method verifies the balancing of parentheses in a string
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def _for(counter: Int, chars: List[Char]): Boolean =
+      if (chars.isEmpty) counter == 0
+      else {
+        val charHead = chars.head
+        if (charHead == ')' && counter == 0) return false
+        val newCounter: Int =
+          if (charHead == '(') counter + 1
+          else if (charHead == ')') counter - 1
+          else counter
+        _for(newCounter, chars.tail)
+      }
+    _for(0, chars)
+  }
 
   /**
    * Exercise 3
